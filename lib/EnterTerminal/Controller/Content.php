136123c67f2ae4cbe44f640304f1d94314e0cd8f
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-07-02 16:48:48 +0400

    TERMINALS-713 добавил атрибуты для URL адресов на контекстных траницах

--- lib/EnterTerminal/Controller/Content.php
+++ lib/EnterTerminal/Controller/Content.php
@@ -2,7 +2,9 @@
 
 namespace EnterTerminal\Controller;
 
+use Enter\Curl\Client;
 use Enter\Http;
+use EnterSite\ViewHelperTrait;
 use EnterTerminal\ConfigTrait;
 use EnterSite\CurlClientTrait;
 use EnterSite\Controller;
@@ -11,7 +13,7 @@ use EnterSite\Curl\Query;
 use EnterTerminal\Model\Page\Content as Page;
 
 class Content {
-    use ConfigTrait, CurlClientTrait {
+    use ConfigTrait, CurlClientTrait, ViewHelperTrait {
         ConfigTrait::getConfig insteadof CurlClientTrait;
     }
 
@@ -21,10 +23,8 @@ class Content {
      * @return Http\JsonResponse
      */
     public function execute(Http\Request $request) {
-        $config = $this->getConfig();
         $curl = $this->getCurlClient();
 
-        /*
         // ид магазина
         $shopId = (new Repository\Shop())->getIdByHttpRequest($request);
 
@@ -39,7 +39,6 @@ class Content {
         if (!$shop) {
             throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
         }
-        */
 
         $contentToken = $request->query['contentToken'];
         if (!$contentToken) {
@@ -55,9 +54,80 @@ class Content {
         $page = new Page();
 
         $item = $contentItemQuery->getResult();
-        $page->content = preg_replace('/<a(\s+[^>]*href="http:\/\/www\.enter\.ru\/)([^"\s]*)/i', '<a$1$2" data-content-token="$2"', $item['content']);
+        $page->content = $this->processContentLinks($item['content'], $curl, $shop->regionId);
         $page->title = isset($item['title']) ? $item['title'] : null;
 
         return new Http\JsonResponse($page);
+    }
+
+    private function processContentLinks($content, Client $curl, $regionId) {
+        if (preg_match_all('/<a\s+[^>]*href="((?:http:\/\/www\.enter\.ru)?\/[^"]*)"/i', $content, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE)) {
+            foreach ($matches as $key => $match) {
+                $url = $this->getUrlFromHrefAttributeValue($match[1][0]);
+
+                if (strpos($url, '/catalog/') === 0) {
+                    $matches[$key]['query'] = new Query\Product\Category\GetItemByToken($this->getTokenByUrl($url), $regionId);
+                    $curl->prepare($matches[$key]['query']);
+                }
+                else if (strpos($url, '/product/') === 0) {
+                    $matches[$key]['query'] = new Query\Product\GetItemByToken($this->getTokenByUrl($url), $regionId);
+                    $curl->prepare($matches[$key]['query']);
+                }
+            }
+
+            $curl->execute();
+            $categoryRepository = new \EnterSite\Repository\Product\Category();
+            $productRepository = new \EnterSite\Repository\Product();
+
+            $shift = 0;
+            foreach ($matches as $match) {
+                $linkTagEndPos = $match[0][1] + strlen($match[0][0]) + $shift;
+                $url = $this->getUrlFromHrefAttributeValue($match[1][0]);
+
+                if (strpos($url, '/catalog/') === 0) {
+                    $category = $categoryRepository->getObjectByQuery($match['query']);
+                    $newAttributes = ' data-type="ProductCatalog/Category" data-category-id="' . $this->getViewHelper()->escape($category->id) . '"';
+                }
+                else if (strpos($url, '/product/') === 0) {
+                    $product = $productRepository->getObjectByQuery($match['query']);
+                    $newAttributes = ' data-type="ProductCard" data-product-id="' . $this->getViewHelper()->escape($product->id) . '"';
+                }
+                else if (strpos($url, 'http://www.enter.ru/') === 0) {
+                    $newAttributes = ' data-type="Content" data-content-token="' . $this->getTokenByUrl($url) . '"';
+                }
+                else {
+                    $newAttributes = null;
+                }
+
+                if ($newAttributes !== null) {
+                    $content = substr($content, 0, $linkTagEndPos) . $newAttributes . substr($content, $linkTagEndPos);
+                    $shift += strlen($newAttributes);
+                }
+            }
+        }
+
+        return $content;
+    }
+
+    private function getUrlFromHrefAttributeValue($href) {
+        $url = html_entity_decode($href);
+        $url = preg_replace('/\?.*$|\#.*$/s', '', $url);
+        return $url;
+    }
+
+    private function getTokenByUrl($url) {
+        if (strpos($url, 'http://www.enter.ru/') === 0) {
+            $url = substr($url, strlen('http://www.enter.ru/'));
+        }
+        else if (strpos($url, '/') === 0) {
+            $url = substr($url, 1);
+        }
+
+        $segments = explode('/', $url);
+        // TODO: добавить поддержку для путей из 4х сегментов
+        if (count($segments) <= 3)
+            return end($segments);
+
+        return null;
     }
 }
