6f1e1bfb0ea5a08b7cc42ec5c26156ab9fbe0a6f
g84 <green840429@gmail.com>
2014-07-28 15:48:25 +0400

    TERMINALS-? рефакторинг: удалил модели страниц

--- lib/EnterTerminal/Controller/Config.php
+++ lib/EnterTerminal/Controller/Config.php
@@ -1,54 +1,67 @@
 <?php
 
-namespace EnterTerminal\Controller;
+namespace EnterTerminal\Controller {
 
-use Enter\Http;
-use EnterAggregator\CurlTrait;
-use EnterQuery as Query;
-use EnterTerminal\Model\Page\Config as Page;
+    use Enter\Http;
+    use EnterAggregator\CurlTrait;
+    use EnterQuery as Query;
+    use EnterTerminal\Controller\Config\Response;
 
-class Config {
-    use CurlTrait;
+    class Config {
+        use CurlTrait;
 
-    /**
-     * @param Http\Request $request
-     * @throws \Exception
-     * @return Http\JsonResponse
-     */
-    public function execute(Http\Request $request) {
-        $curl = $this->getCurl();
+        /**
+         * @param Http\Request $request
+         * @throws \Exception
+         * @return Http\JsonResponse
+         */
+        public function execute(Http\Request $request) {
+            $curl = $this->getCurl();
 
-        if (!is_string($request->query['ip'])) {
-            throw new \Exception('Параметр ip должен быть строкой');
+            if (!is_string($request->query['ip'])) {
+                throw new \Exception('Параметр ip должен быть строкой');
+            }
+
+            $infoQuery = new Query\Terminal\GetInfoByIp($request->query['ip']);
+            $curl->prepare($infoQuery);
+
+            $curl->execute();
+
+            // ответ
+            $response = new Response();
+
+            $data = $infoQuery->getResult();
+            if (!$data) {
+                throw new \Exception('Не удалось получить конфигурацию терминала');
+            }
+            $shopId = $response->info['shop_id'];
+
+            $response->info = $data;
+
+            $shopQuery = new Query\Shop\GetItemById($shopId);
+            $curl->prepare($shopQuery);
+
+            $curl->execute();
+
+            $shop = (new \EnterTerminal\Repository\Shop())->getObjectByQuery($shopQuery);
+            if (!$shop) {
+                throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
+            }
+
+            $response->shop = $shop;
+
+            return new Http\JsonResponse($response);
         }
+    }
+}
 
-        $infoQuery = new Query\Terminal\GetInfoByIp($request->query['ip']);
-        $curl->prepare($infoQuery);
+namespace EnterTerminal\Controller\Config {
+    use EnterModel as Model;
 
-        $curl->execute();
-
-        $page = new Page();
-
-        $data = $infoQuery->getResult();
-        if (!$data) {
-            throw new \Exception('Не удалось получить конфигурацию терминала');
-        }
-        $shopId = $page->info['shop_id'];
-
-        $page->info = $data;
-
-        $shopQuery = new Query\Shop\GetItemById($shopId);
-        $curl->prepare($shopQuery);
-
-        $curl->execute();
-
-        $shop = (new \EnterTerminal\Repository\Shop())->getObjectByQuery($shopQuery);
-        if (!$shop) {
-            throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
-        }
-
-        $page->shop = $shop;
-
-        return new Http\JsonResponse($page);
+    class Response {
+        /** @var \EnterModel\Shop */
+        public $shop;
+        /** @var array */
+        public $info;
     }
 }
--- lib/EnterTerminal/Controller/ProductCatalog/Category.php
+++ lib/EnterTerminal/Controller/ProductCatalog/Category.php
@@ -1,97 +1,117 @@
 <?php
 
-namespace EnterTerminal\Controller\ProductCatalog;
+namespace EnterTerminal\Controller\ProductCatalog {
 
-use Enter\Http;
-use EnterTerminal\ConfigTrait;
-use EnterAggregator\CurlTrait;
-use EnterAggregator\Model\Context;
-use EnterTerminal\Controller;
-use EnterQuery as Query;
-use EnterModel as Model;
-use EnterTerminal\Model\Page\ProductCatalog\Category as Page;
+    use Enter\Http;
+    use EnterTerminal\ConfigTrait;
+    use EnterAggregator\CurlTrait;
+    use EnterAggregator\Model\Context;
+    use EnterTerminal\Controller;
+    use EnterQuery as Query;
+    use EnterModel as Model;
+    use EnterTerminal\Controller\ProductCatalog\Category\Response;
 
-class Category {
-    use ConfigTrait, CurlTrait;
+    class Category {
+        use ConfigTrait, CurlTrait;
 
-    /**
-     * @param Http\Request $request
-     * @throws \Exception
-     * @return Http\Response
-     */
-    public function execute(Http\Request $request) {
-        $config = $this->getConfig();
-        $curl = $this->getCurl();
-        $filterRepository = new \EnterTerminal\Repository\Product\Filter();
+        /**
+         * @param Http\Request $request
+         * @throws \Exception
+         * @return Http\Response
+         */
+        public function execute(Http\Request $request) {
+            $config = $this->getConfig();
+            $curl = $this->getCurl();
+            $filterRepository = new \EnterTerminal\Repository\Product\Filter();
 
-        // ид магазина
-        $shopId = (new \EnterTerminal\Repository\Shop())->getIdByHttpRequest($request); // FIXME
+            // ид магазина
+            $shopId = (new \EnterTerminal\Repository\Shop())->getIdByHttpRequest($request); // FIXME
 
-        // ид категории
-        $categoryId = trim((string)$request->query['categoryId']);
-        if (!$categoryId) {
-            throw new \Exception('Не указан параметр categoryId');
+            // ид категории
+            $categoryId = trim((string)$request->query['categoryId']);
+            if (!$categoryId) {
+                throw new \Exception('Не указан параметр categoryId');
+            }
+
+            // номер страницы
+            $pageNum = (int)$request->query['page'] ?: 1;
+
+            // количество товаров на страницу
+            $limit = (int)$request->query['limit'] ?: 10;
+
+            // сортировка
+            $sorting = null;
+            if (!empty($request->query['sort']['token']) && !empty($request->query['sort']['direction'])) {
+                $sorting = new Model\Product\Sorting();
+                $sorting->token = trim((string)$request->query['sort']['token']);
+                $sorting->direction = trim((string)$request->query['sort']['direction']);
+            }
+
+            // запрос магазина
+            $shopItemQuery = new Query\Shop\GetItemById($shopId);
+            $curl->prepare($shopItemQuery);
+
+            $curl->execute();
+
+            // магазин
+            $shop = (new \EnterRepository\Shop())->getObjectByQuery($shopItemQuery);
+            if (!$shop) {
+                throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
+            }
+
+            // фильтры в запросе
+            $requestFilters = $filterRepository->getRequestObjectListByHttpRequest($request);
+            // фильтр категории в http-запросе
+            //$requestFilters[] = $filterRepository->getRequestObjectByCategory($category);
+
+            $context = new Context\ProductCatalog();
+            $context->mainMenu = false;
+            $context->parentCategory = true;
+            $controllerResponse = (new \EnterAggregator\Controller\ProductCatalog\ChildCategory())->execute(
+                $shop->regionId,
+                ['id' => $categoryId], // критерий получения категории товара
+                $pageNum, // номер страницы
+                $limit, // лимит
+                $sorting, // сортировка
+                $filterRepository, // репозиторий фильтров
+                $requestFilters, // фильтры в http-запросе
+                $context
+            );
+
+            // категория
+            if (!$controllerResponse->category) {
+                return (new Controller\Error\NotFound())->execute($request, sprintf('Категория товара #%s не найдена', $categoryId));
+            }
+
+            // ответ
+            $response = new Response();
+            $response->category = $controllerResponse->category;
+            $response->catalogConfig = $controllerResponse->catalogConfig;
+            $response->products = $controllerResponse->products;
+            $response->productCount = $controllerResponse->productIdPager->count;
+            $response->filters = $controllerResponse->filters;
+            $response->sortings = $controllerResponse->sortings;
+
+            return new Http\JsonResponse($response);
         }
+    }
+}
 
-        // номер страницы
-        $pageNum = (int)$request->query['page'] ?: 1;
+namespace EnterTerminal\Controller\ProductCatalog\Category {
+    use EnterModel as Model;
 
-        // количество товаров на страницу
-        $limit = (int)$request->query['limit'] ?: 10;
-
-        // сортировка
-        $sorting = null;
-        if (!empty($request->query['sort']['token']) && !empty($request->query['sort']['direction'])) {
-            $sorting = new Model\Product\Sorting();
-            $sorting->token = trim((string)$request->query['sort']['token']);
-            $sorting->direction = trim((string)$request->query['sort']['direction']);
-        }
-
-        // запрос магазина
-        $shopItemQuery = new Query\Shop\GetItemById($shopId);
-        $curl->prepare($shopItemQuery);
-
-        $curl->execute();
-
-        // магазин
-        $shop = (new \EnterRepository\Shop())->getObjectByQuery($shopItemQuery);
-        if (!$shop) {
-            throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
-        }
-
-        // фильтры в запросе
-        $requestFilters = $filterRepository->getRequestObjectListByHttpRequest($request);
-        // фильтр категории в http-запросе
-        //$requestFilters[] = $filterRepository->getRequestObjectByCategory($category);
-
-        $context = new Context\ProductCatalog();
-        $context->mainMenu = false;
-        $context->parentCategory = true;
-        $controllerResponse = (new \EnterAggregator\Controller\ProductCatalog\ChildCategory())->execute(
-            $shop->regionId,
-            ['id' => $categoryId], // критерий получения категории товара
-            $pageNum, // номер страницы
-            $limit, // лимит
-            $sorting, // сортировка
-            $filterRepository, // репозиторий фильтров
-            $requestFilters, // фильтры в http-запросе
-            $context
-        );
-
-        // категория
-        if (!$controllerResponse->category) {
-            return (new Controller\Error\NotFound())->execute($request, sprintf('Категория товара #%s не найдена', $categoryId));
-        }
-
-        // страница
-        $page = new Page();
-        $page->category = $controllerResponse->category;
-        $page->catalogConfig = $controllerResponse->catalogConfig;
-        $page->products = $controllerResponse->products;
-        $page->productCount = $controllerResponse->productIdPager->count;
-        $page->filters = $controllerResponse->filters;
-        $page->sortings = $controllerResponse->sortings;
-
-        return new Http\JsonResponse($page);
+    class Response {
+        /** @var Model\Product\Category */
+        public $category;
+        /** @var Model\Product\Catalog\Config */
+        public $catalogConfig;
+        /** @var Model\Product[] */
+        public $products = [];
+        /** @var int */
+        public $productCount;
+        /** @var Model\Product\Sorting[] */
+        public $sortings = [];
+        /** @var Model\Product\Filter[] */
+        public $filters = [];
     }
 }
