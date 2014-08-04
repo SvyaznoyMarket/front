29b4a909b8e75fa7cc90f81d9b9244f9e9991095
g84 <green840429@gmail.com>
2014-08-04 13:51:37 +0400

    MSITE-? TERMINALS-? обработчик ошибок

--- lib/EnterAggregator/Action/HandleError.php
+++ lib/EnterAggregator/Action/HandleError.php
@@ -7,14 +7,27 @@ class HandleError {
      * @param $error
      */
     public function execute(&$error) {
-        set_error_handler(function($code, $message, $file, $line) use (&$error) {
-            switch ($code) {
-                case E_USER_ERROR:
-                    $error = new \ErrorException($message, 0, $code, $file, $line);
-                    return true;
+        set_error_handler(function($level, $message, $file, $line) use (&$error) {
+            static $levels = [
+                E_WARNING           => 'Warning',
+                E_NOTICE            => 'Notice',
+                E_USER_ERROR        => 'User Error',
+                E_USER_WARNING      => 'User Warning',
+                E_USER_NOTICE       => 'User Notice',
+                E_STRICT            => 'Runtime Notice',
+                E_RECOVERABLE_ERROR => 'Catchable Fatal Error',
+                E_DEPRECATED        => 'Deprecated',
+                E_USER_DEPRECATED   => 'User Deprecated',
+            ];
 
-                case E_WARNING:
-                    throw new \ErrorException($message, 0, $code, $file, $line);
+            switch ($level) {
+                case E_USER_ERROR:
+                    $error = new \ErrorException($message, 0, $level, $file, $line);
+                    return true;
+            }
+
+            if (error_reporting() & $level) {
+                throw new \ErrorException(sprintf('%s: %s in %s line %d', isset($levels[$level]) ? $levels[$level] : $level, $message, $file, $line));
             }
 
             return false;
