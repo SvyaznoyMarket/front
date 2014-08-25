7f1f87b8300b97fdcd753e0c74492f67bfc64851
g84 <green840429@gmail.com>
2014-08-25 13:14:05 +0400

    FRONT-? добавил contentType для изображений

--- lib/EnterModel/Product/Media/Photo.php
+++ lib/EnterModel/Product/Media/Photo.php
@@ -7,10 +7,8 @@ class Photo {
     public $id;
     /** @var string */
     public $source;
-    /** @var int */
-    public $width;
-    /** @var int */
-    public $height;
+    /** @var string */
+    public $contentType;
 
     /**
      * @param array $data
@@ -18,7 +16,8 @@ class Photo {
     public function __construct(array $data = []) {
         if (array_key_exists('id', $data)) $this->id = (string)$data['id'];
         if (array_key_exists('source', $data)) $this->source = (string)$data['source'];
-        if (array_key_exists('width', $data)) $this->width = (int)$data['width'];
-        if (array_key_exists('height', $data)) $this->height = (int)$data['height'];
+        if ($this->source && $extension = pathinfo($this->source, PATHINFO_EXTENSION)) {
+            $this->contentType = 'image/' . $extension;
+        }
     }
 }
