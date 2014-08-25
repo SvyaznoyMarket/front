4d76079c38cddf338fb8e6db94e76f871058523a
g84 <green840429@gmail.com>
2014-08-25 14:06:49 +0400

    FRONT-? media.host

--- lib/EnterModel/Product/Media/Photo.php
+++ lib/EnterModel/Product/Media/Photo.php
@@ -2,13 +2,19 @@
 
 namespace EnterModel\Product\Media;
 
+use EnterAggregator\ConfigTrait;
+
 class Photo {
+    use ConfigTrait; // FIXME!!!
+
     /** @var string */
     public $id;
     /** @var string */
     public $source;
     /** @var string */
     public $contentType;
+    /** @var string */
+    public $host;
 
     /**
      * @param array $data
@@ -18,6 +24,11 @@ class Photo {
         if (array_key_exists('source', $data)) $this->source = (string)$data['source'];
         if ($this->source && $extension = pathinfo($this->source, PATHINFO_EXTENSION)) {
             $this->contentType = 'image/' . $extension;
+
+            $hosts = $this->getConfig()->mediaHosts;
+            $index = !empty($this->id) ? ($this->id % count($hosts)) : rand(0, count($hosts) - 1);
+
+            $this->host =  isset($hosts[$index]) ? $hosts[$index] : null;
         }
     }
 }
