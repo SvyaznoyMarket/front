5338e509233a12a926b8209de9f4dd091f19d375
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-09-16 19:16:56 +0400

    TERMINALS-874 Категория Авто без идентификатора

--- lib/EnterRepository/MainMenu.php
+++ lib/EnterRepository/MainMenu.php
@@ -67,6 +67,10 @@ class MainMenu {
                         $element = new Model\MainMenu\Element($elementItem);
                         $element->type = 'category';
                         $element->id = (string)$categoryItem['id'];
+                        if (!$element->id && isset($elementItem['source']['id'])) {
+                            $element->id = (string)$elementItem['source']['id'];
+                        }
+
                         if (!$element->name) {
                             $element->name = (string)$categoryItem['name'];
                         }
