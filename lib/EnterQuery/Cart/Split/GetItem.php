600e2ee007a9be6451ce24b5a45f09c600976959
g84 <green840429@gmail.com>
2014-10-02 17:54:50 +0400

    FRONT-? пофиксил cart/split?shopId=...

--- lib/EnterQuery/Cart/Split/GetItem.php
+++ lib/EnterQuery/Cart/Split/GetItem.php
@@ -59,12 +59,10 @@ class GetItem extends Query {
             $this->data['previous_split'] = $previousSplit;
             $this->data['changes'] = $change;
         } else {
-            $this->data = [
-                'cart' => [
-                    'product_list' => array_values(array_map(function(Model\Cart\Product $cartProduct) {
-                        return array_merge(['quantity' => $cartProduct->quantity], $cartProduct->id ? ['id' => $cartProduct->id] : ['ui' => $cartProduct->ui]);
-                    }, $cart->product)),
-                ],
+            $this->data['cart'] = [
+                'product_list' => array_values(array_map(function(Model\Cart\Product $cartProduct) {
+                    return array_merge(['quantity' => $cartProduct->quantity], $cartProduct->id ? ['id' => $cartProduct->id] : ['ui' => $cartProduct->ui]);
+                }, $cart->product)),
             ];
         }
 
