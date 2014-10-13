9ccc4fc431ee2d898fbafd6bffcce76bec81bbf3
tamili <ta.burlakova@gmail.com>
2014-10-13 15:40:34 +0400

    MSITE-90 Новое оформление заказа

--- web/js/module/order.common.js
+++ web/js/module/order.common.js
@@ -9,56 +9,63 @@ define(
     		orderBtn = $('.js-order-btn'),
     		deliveryType = $('.js-delivery-type-name'),
     		deliveryTypeBox = $('.js-delivery-type');
+    	// end of vars
 
-    	var orderBoxSwitch = function orderBoxSwitch( event ) {
-			event.preventDefault();
+    	var 
+    		// открываем блоки в оформлении заказа
+	    	orderBoxSwitch = function orderBoxSwitch( event ) {
+				event.preventDefault();
 
-			var $self = $(this),
-				orderBtnData = $self.data('btn'),
-				orderBox = orderWrap.filter('[data-box="data-'+orderBtnData+'"]');
-				orderBoxNot = orderWrap.not('[data-box="data-'+orderBtnData+'"]');
+				var $self = $(this),
+					orderBtnData = $self.data('btn'),
+					orderBox = orderWrap.filter('[data-box="data-'+orderBtnData+'"]');
+					orderBoxNot = orderWrap.not('[data-box="data-'+orderBtnData+'"]');
 
-			orderBoxNot.removeClass('box-show');
-			orderBox.toggleClass('box-show');
-		},
+				orderBoxNot.removeClass('box-show');
+				orderBox.toggleClass('box-show');
+			},
 
-		orderBoxClose = function orderBoxClose () {
-			var $self = $(this);
+			// закрываем блоки в оформлении заказа
+			orderBoxClose = function orderBoxClose () {
+				var $self = $(this);
 
-			$self.closest('.js-order-box').removeClass('box-show');
-		},
+				$self.closest('.js-order-box').removeClass('box-show');
+			},
 
-		showPopupBox = function showPopupBox(e) {
-            e.stopPropagation();
+			// попап оплаты
+			showPopupBox = function showPopupBox(e) {
+	            e.stopPropagation();
 
-            $('.js-popup-box').enterPopup({
-            	popupCSS : {'max-width' : 300},
-            	closeSelector: ".popupFl_clsr"
-            });
+	            $('.js-popup-box').enterPopup({
+	            	popupCSS : {'max-width' : 300},
+	            	closeSelector: ".popupFl_clsr"
+	            });
 
-            e.preventDefault();
-        },
+	            e.preventDefault();
+	        },
 
+	        // попап календаря
+			showPopupBoxCelendar = function showPopupBoxCelendar(e) {
+	            e.stopPropagation();
 
-		showPopupBoxCelendar = function showPopupBoxCelendar(e) {
-            e.stopPropagation();
+	            $('.js-popup-box-celendar').enterPopup({
+	            	popupCSS : {'max-width' : 308},
+	            	closeSelector: ".popupFl_clsr"
+	            });
+	        },
 
-            $('.js-popup-box-celendar').enterPopup({
-            	popupCSS : {'max-width' : 308},
-            	closeSelector: ".popupFl_clsr"
-            });
-        },
+	        // выбор способа доставки
+			changeDeliveryType = function changeDeliveryType() {
+				var $self = $(this),
+					typeId = $self.attr('data-type');
 
-		changeDeliveryType = function changeDeliveryType() {
-			var $self = $(this),
-				typeId = $self.attr('data-type');
+				deliveryType.removeClass('orderCol_mode_i-act');
+				deliveryTypeBox.removeClass('box-show');
 
-			deliveryType.removeClass('orderCol_mode_i-act');
-			deliveryTypeBox.removeClass('box-show');
-
-			$self.addClass('orderCol_mode_i-act');
-			$("#"+typeId).addClass('box-show');		
-		};
+				$self.addClass('orderCol_mode_i-act');
+				$("#"+typeId).addClass('box-show');		
+			};
+		// end of functions
 
 		orderBtn.on('click', orderBoxSwitch);
 		orderClsr.on('click', orderBoxClose);
