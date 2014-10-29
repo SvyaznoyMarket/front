35684e8eda3f6cc4cb1da74a358b151e21ac1a4d
g84 <green.840429@gmail.com>
2014-10-29 17:19:29 +0300

    TERMINALS-938 Отправка номера заказа на номер телефона

---
+++ lib/EnterQuery/Order/SendNumber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EnterQuery\Order;
+
+use Enter\Curl\Query;
+use EnterQuery\CoreQueryTrait;
+use EnterQuery\Url;
+
+class SendNumber extends Query {
+    use CoreQueryTrait;
+
+    /** @var array|null */
+    protected $result;
+
+    /**
+     * @param $orderNumber
+     * @param $notificationType
+     * @param string|null $phone
+     * @param string|null $token
+     */
+    public function __construct($orderNumber, $notificationType, $phone = null, $token = null) {
+        $this->url = new Url();
+        $this->url->path = 'order/send-number';
+        $this->url->query = [
+            'order_number'      => $orderNumber,
+            'notification_type' => $notificationType,
+        ];
+        if ($phone) {
+            $this->url->query['phone_number'] = $phone;
+        }
+        if ($token) {
+            $this->url->query['token'] = $token;
+        }
+
+        $this->init();
+    }
+
+    /**
+     * @param $response
+     */
+    public function callback($response) {
+        $this->result = $this->parse($response);
+    }
+}
---
+++ lib/EnterTerminal/Controller/Order/SendNumber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace EnterTerminal\Controller\Order;
+
+use Enter\Http;
+use EnterTerminal\ConfigTrait;
+use EnterAggregator\CurlTrait;
+use EnterQuery as Query;
+
+class SendNumber {
+    use ConfigTrait, CurlTrait;
+
+    /**
+     * @param Http\Request $request
+     * @throws \Exception
+     * @return Http\JsonResponse
+     */
+    public function execute(Http\Request $request) {
+        $config = $this->getConfig();
+        $curl = $this->getCurl();
+
+        $orderNumber = is_scalar($request->query['orderNumber']) ? $request->query['orderNumber'] : null;
+        if (!$orderNumber) {
+            throw new \Exception('Параметр orderNumber должен быть строкой');
+        }
+
+        $notificationType = is_scalar($request->query['notificationType']) ? $request->query['notificationType'] : null;
+
+        $phone = is_scalar($request->query['phone']) ? $request->query['phone'] : null;
+
+        // TODO
+        $token = null;
+
+        $sendQuery = new Query\Order\SendNumber($orderNumber, $notificationType, $phone, $token);
+        $sendQuery->setTimeout(10 * $config->coreService->timeout);
+        $curl->prepare($sendQuery)->execute();
+
+        $responseData = [];
+
+        if ($sendQuery->getError()) {
+            $responseData['error'] = [
+                'code'    => $sendQuery->getError()->getCode(),
+                'message' => $sendQuery->getError()->getMessage(),
+            ];
+        } else {
+            $responseData = $sendQuery->getResult();
+        }
+
+        return new Http\JsonResponse($responseData);
+    }
+}
