e251c06a9df82163629d3a3239d3d069ed16b215
g84 <green.840429@gmail.com>
2014-12-10 16:02:43 +0300

    AG-? исправление

--- lib/EnterAggregator/Controller/Order/Create.php
+++ lib/EnterAggregator/Controller/Order/Create.php
@@ -117,7 +117,7 @@ namespace EnterAggregator\Controller\Order {
 
             $orderProductsById = [];
             foreach ($orders as $order) {
-                foreach ($order->product as $orderProduct) {
+                foreach ((array)$order->product as $orderProduct) {
                     $orderProductsById[$orderProduct->id] = $orderProduct;
                 }
             }
@@ -140,7 +140,7 @@ namespace EnterAggregator\Controller\Order {
 
             // товары
             foreach ($orders as $order) {
-                foreach ($order->product as $i => $orderProduct) {
+                foreach ((array)$order->product as $i => $orderProduct) {
                     $product = isset($productsById[$orderProduct->id]) ? $productsById[$orderProduct->id] : null;
                     if (!$product) continue;
 
