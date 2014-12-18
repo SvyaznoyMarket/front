439fc62ba0ddba247acb81c9e23aa242c81190ec
g84 <green.840429@gmail.com>
2014-12-18 12:16:31 +0300

    AG-? создание купона

---
+++ lib/EnterMobileApplication/Controller/Coupon/Create.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace EnterMobileApplication\Controller\Coupon {
+
+    use Enter\Http;
+    use EnterMobileApplication\ConfigTrait;
+    use EnterAggregator\CurlTrait;
+    use EnterAggregator\LoggerTrait;
+    use EnterQuery as Query;
+    use EnterModel as Model;
+    use EnterMobileApplication\Controller\Coupon\Create\Response;
+
+    class Create {
+        use ConfigTrait, LoggerTrait, CurlTrait;
+
+        /**
+         * @param Http\Request $request
+         * @throws \Exception
+         * @return Http\JsonResponse
+         */
+        public function execute(Http\Request $request) {
+            $config = $this->getConfig();
+            $curl = $this->getCurl();
+
+            // ответ
+            $response = new Response();
+
+            $token = is_scalar($request->query['token']) ? (string)$request->query['token'] : null;
+            if (!$token) {
+                //throw new \Exception('Не указан token');
+            }
+
+            $couponSeriesId = is_scalar($request->query['couponSeriesId']) ? (string)$request->query['couponSeriesId'] : null;
+            if (!$couponSeriesId) {
+                throw new \Exception('Не указан couponSeriesId', 400);
+            }
+            $phone = $request->data['phone'];
+            $email = $request->data['email'];
+            $promoToken = is_scalar($request->data['promoToken']) ? (string)$request->data['promoToken'] : null;
+            if (!$promoToken) {
+                throw new \Exception('Не указан promoToken', 400);
+            }
+
+            $couponSeries = new Model\Coupon\Series();
+            $couponSeries->id = $couponSeriesId;
+
+            // запрос пользователя
+            $userItemQuery = $token ? new Query\User\GetItemByToken($token) : null;
+            if ($userItemQuery) {
+                $curl->prepare($userItemQuery)->execute();
+            }
+
+            // получение пользователя
+            $user = $userItemQuery ? (new \EnterRepository\User())->getObjectByQuery($userItemQuery) : null;
+            if ($user) {
+                $response->token = $token;
+            } else {
+                $user = new Model\User();
+                $user->email = $email;
+                $user->phone = $phone;
+            }
+
+            if (!$user->email && !$user->phone) {
+                throw new \Exception('Не указаны телефон или email', 400);
+            }
+
+            $createQuery = new Query\Coupon\Create($couponSeries, $user, $promoToken);
+            $createQuery->setTimeout(10 * $config->coreService->timeout);
+
+            $curl->query($createQuery);
+
+            try {
+                $result = $createQuery->getResult();
+            } catch (\EnterQuery\CoreQueryException $e) {
+                $detail = $e->getDetail();
+
+                if (600 == $e->getCode()) {
+                    foreach ($detail as $fieldName => $errors) {
+                        foreach ((array)$errors as $errorType => $errorMessage) {
+                            switch ($fieldName) {
+                                case 'name':
+                                    $response->errors[] = ['code' => $e->getCode(), 'message' => ('isEmpty' === $errorType) ? 'Не заполнено имя' : 'Некорректно введено имя', 'field' => 'name'];
+                                    break;
+                                case 'mobile':
+                                    $response->errors[] = ['code' => $e->getCode(), 'message' => ('isEmpty' === $errorType) ? 'Не заполнен номер телефона' : 'Некорректно введен номер телефона', 'field' => 'phone'];
+                                    break;
+                                case 'email':
+                                    $response->errors[] = ['code' => $e->getCode(), 'message' => ('isEmpty' === $errorType) ? 'Не заполнен email' : 'Некорректно введен email', 'field' => 'email'];
+                                    break;
+                                case 'guid':
+                                    $response->errors[] = ['code' => $e->getCode(), 'message' => ('isEmpty' === $errorType) ? 'Не передан идентификатор серии купона' : 'Невалидный идентификатор серии купона', 'field' => 'couponSeriesId'];
+                                    break;
+                                default:
+                                    $response->errors[] = ['code' => $e->getCode(), 'message' => 'Неизвестная ошибка', 'field' => null];
+                            }
+                        }
+                    }
+                } else {
+                    throw $e;
+                }
+            }
+
+            // response
+            return new Http\JsonResponse($response);
+        }
+    }
+}
+
+namespace EnterMobileApplication\Controller\Coupon\Create {
+    use EnterModel as Model;
+
+    class Response {
+        /** @var string|null */
+        public $token;
+         /** @var array[] */
+        public $errors = [];
+    }
+}
---
+++ lib/EnterQuery/Coupon/Create.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace EnterQuery\Coupon;
+
+use Enter\Curl\Query;
+use EnterQuery\CoreQueryTrait;
+use EnterQuery\Url;
+use EnterModel as Model;
+
+class Create extends Query {
+    use CoreQueryTrait;
+
+    /** @var array|null */
+    protected $result;
+
+    public function __construct(Model\Coupon\Series $couponSeries, Model\User $user, $promoToken = null) {
+        $this->url = new Url();
+        $this->url->path = 'v2/coupon/send';
+
+        $this->url->query = [];
+        $this->data = [
+            'guid' => $couponSeries->id,
+        ];
+        if ($promoToken) {
+            $this->data['promo'] = $promoToken;
+        }
+        if ($user->email) {
+            $this->data['email'] = $user->email;
+        }
+        if ($user->phone) {
+            $this->data['mobile'] = $user->phone;
+        }
+        if ($user->ui) {
+            $this->data['user_uid'] = $user->ui;
+        }
+
+        $this->init();
+    }
+
+    /**
+     * @param $response
+     */
+    public function callback($response) {
+        $data = $this->parse($response);
+
+        $this->result = $data;
+    }
+}
