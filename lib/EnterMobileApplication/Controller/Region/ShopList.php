5b488c1b70293cc01ee69b0bb6d0f6d558a3d112
g84 <green.840429@gmail.com>
2014-12-23 18:49:48 +0300

    AG-31 Для региона - Королев не возвращается список магазинов

--- lib/EnterMobileApplication/Controller/Region/ShopList.php
+++ lib/EnterMobileApplication/Controller/Region/ShopList.php
@@ -25,39 +25,31 @@ class ShopList {
         // ид региона
         $filteredRegionId = is_scalar($request->query['regionId']) ? (string)$request->query['regionId'] : null;
 
-        $regionListQuery = new Query\Region\GetMainList();
-        $curl->prepare($regionListQuery);
+        $firstRegionIds = ['14974', '108136'];
 
         $shopListQuery = new Query\Shop\GetList();
         $curl->prepare($shopListQuery);
 
         $curl->execute();
 
-        $firstRegionDataById = [];
         $regionDataById = [];
-        foreach ($regionListQuery->getResult() as $regionItem) {
-            $regionId = (string)$regionItem['id'];
-
-            if ($filteredRegionId && ($regionId !== $filteredRegionId)) continue;
-
-            $regionItem = [
-                'id'    => $regionId,
-                'name'  => (string)$regionItem['name'],
-                'shops' => [],
-            ];
-
-            if (in_array($regionId, ['14974', '108136'])) {
-                $firstRegionDataById[$regionId] = $regionItem;
-            } else {
-                $regionDataById[$regionId] = $regionItem;
-            }
-        }
-
-        $regionDataById = $firstRegionDataById + $regionDataById;
 
         foreach ($shopListQuery->getResult() as $shopItem) {
             $regionId = !empty($shopItem['geo']['id']) ? (string)$shopItem['geo']['id'] : null;
-            if (!$regionId || !isset($regionDataById[$regionId])) continue;
+            if (
+                !$regionId
+                || ($filteredRegionId && ($regionId !== $filteredRegionId))
+            ) {
+                continue;
+            }
+
+            if (!isset($regionDataById[$regionId])) {
+                $regionDataById[$regionId] = [
+                    'id'    => $regionId,
+                    'name'  => @$shopItem['geo']['name'] ? (string)$shopItem['geo']['name'] : null,
+                    'shops' => [],
+                ];
+            }
 
             $shopItem = [
                 'id'        => (string)@$shopItem['id'],
@@ -73,6 +65,20 @@ class ShopList {
 
             $regionDataById[$regionId]['shops'][] = $shopItem;
         }
+
+        try {
+            usort($regionDataById, function($a, $b) use (&$firstRegionIds) {
+                if (in_array($a['id'], $firstRegionIds)) {
+                    return -1;
+                } else if (in_array($b['id'], $firstRegionIds)) {
+                    return 1;
+                } else if ($a['name'] == $b['name']) {
+                    return 0;
+                }
+
+                return $a['name'] < $b['name'] ? -1 : 1;
+            });
+        } catch (\Exception $e) {}
 
         $response['regions'] = array_values(array_filter($regionDataById, function($regionItem) {
             return (bool)$regionItem['shops'];
