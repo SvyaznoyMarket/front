5bb3dcee2f134a86b96f642cd564962f4571c641
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-02-25 18:56:49 +0300

    MSITE-132 Временно исключить из выдачи сайта партнёрские товары-слоты

--- lib/EnterQuery/Product/GetIdPager.php
+++ lib/EnterQuery/Product/GetIdPager.php
@@ -32,6 +32,9 @@ class GetIdPager extends Query {
             }
         }
 
+        // MSITE-132 Временно исключить из выдачи сайта партнёрские товары-слоты
+        $filterData[] = ['exclude_partner_type', 1, \EnterModel\Product::PARTNER_OFFER_TYPE_SLOT];
+
         $this->url = new Url();
         $this->url->path = 'v2/listing/list';
         $this->url->query = [
