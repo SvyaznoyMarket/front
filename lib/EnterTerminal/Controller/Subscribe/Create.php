561da04af4b86460ec4b2fc310fb5349f7874f8a
g84 <green.840429@gmail.com>
2015-02-26 16:40:26 +0300

    TERMINALS-1041 Метод подписки для терминала

---
+++ lib/EnterTerminal/Controller/Subscribe/Create.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EnterTerminal\Controller\Subscribe;
+
+use Enter\Http;
+use EnterAggregator\CurlTrait;
+use EnterQuery as Query;
+use EnterModel as Model;
+
+class Create {
+    use CurlTrait;
+
+    /**
+     * @param Http\Request $request
+     * @throws \Exception
+     * @return Http\JsonResponse
+     */
+    public function execute(Http\Request $request) {
+        $curl = $this->getCurl();
+
+        if (!$email = $request->data['email']) {
+            throw new \Exception('Не передан email', Http\Response::STATUS_BAD_REQUEST);
+        }
+
+        if (!$channelId = $request->data['channelId']) {
+            throw new \Exception('Не передан channelId', Http\Response::STATUS_BAD_REQUEST);
+        }
+
+        $subscribe = new Model\Subscribe();
+        $subscribe->email = $email;
+        $subscribe->channelId = $channelId;
+
+        $query = new Query\Subscribe\CreateItem(
+            $subscribe
+        );
+        $curl->prepare($query);
+
+        $curl->execute();
+
+        $responseData = [];
+        try {
+            $query->getResult();
+        } catch (\Exception $e) {
+            switch ($e->getCode()) {
+                case 910:
+                    $responseData['error'] = ['code' => $e->getCode(), 'message' => 'Подписка уже оформлена'];
+                    break;
+            }
+        }
+
+        // ответ
+        return new Http\JsonResponse($responseData);
+    }
+}
