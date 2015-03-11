ae47ea48086510b507db8186b8e8b299d0c53530
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-03-11 13:16:12 +0300

    MSITE-115 исправил форматирование цен JavaScript'ом

--- web/js/module/util.js
+++ web/js/module/util.js
@@ -3,8 +3,22 @@ define(
     function () {
 
         return {
-            formatCurrency: function (value) {
-                return value.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+            formatCurrency: function (price) {
+                price = String(price);
+                price = price.replace(',', '.');
+                price = price.replace(/\s/g, '');
+                price = String(Number(price).toFixed(2));
+                price = price.split('.');
+
+                if (price[0].length >= 5) {
+                    price[0] = price[0].replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '); // В качестве разделителя груп цифр используется символ &thinsp;
+                }
+
+                if (price[1] == 0) {
+                    price = price.slice(0, 1);
+                }
+
+                return price.join('.');
             }
         };
 
