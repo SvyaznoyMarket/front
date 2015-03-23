d230921f71fe8d95641b9c24c994180cbff070c4
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-03-23 13:39:28 +0300

    TAPI-1 Включить поддержку кухонь

--- lib/EnterQuery/Search/GetItemByPhrase.php
+++ lib/EnterQuery/Search/GetItemByPhrase.php
@@ -42,9 +42,6 @@ class GetItemByPhrase extends Query {
             $this->url->query['product'] = ['sort' => [$sorting->token => $sorting->direction]];
         }
 
-        // AG-59 Кухни по слотам
-        $this->url->query['filter']['filters'][] = ['exclude_partner_type', 1, 2];
-
         $this->init();
     }
 
