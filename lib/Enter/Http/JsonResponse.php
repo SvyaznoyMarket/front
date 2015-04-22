94accae991dab0e606b07da3b3a697ebc6767789
g84 <green.840429@gmail.com>
2015-04-22 15:41:53 +0300

    MAPI-? исправил JsonResponse

--- lib/Enter/Http/JsonResponse.php
+++ lib/Enter/Http/JsonResponse.php
@@ -5,9 +5,13 @@ namespace Enter\Http;
 class JsonResponse extends Response {
     /** @var \ArrayObject */
     public $data;
+    /** @var int */
+    public $encodeOption;
 
     public function __construct($data = null, $statusCode = self::STATUS_OK) {
         parent::__construct(null, $statusCode);
+
+        $this->encodeOption = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
 
         $this->data = new \ArrayObject($data ?: []);
 
@@ -18,7 +22,7 @@ class JsonResponse extends Response {
      * @return $this
      */
     public function sendContent() {
-        $this->content = json_encode($this->data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+        $this->content = json_encode($this->data, $this->encodeOption);
 
         return parent::sendContent();
     }
