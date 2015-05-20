00dc8023513c85c03ed5c0129a0332f548c0122c
g84 <green.840429@gmail.com>
2015-05-20 19:59:10 +0300

    AG-? перенес разбиение в контроллер агрегатора

--- lib/EnterMobileApplication/Controller/Cart/Split.php
+++ lib/EnterMobileApplication/Controller/Cart/Split.php
@@ -4,16 +4,13 @@ namespace EnterMobileApplication\Controller\Cart {
 
     use Enter\Http;
     use EnterMobileApplication\ConfigTrait;
-    use EnterAggregator\CurlTrait;
-    use EnterAggregator\LoggerTrait;
     use EnterAggregator\SessionTrait;
-    use EnterQuery as Query;
     use EnterModel as Model;
     use EnterMobileApplication\Controller;
     use EnterMobileApplication\Controller\Cart\Split\Response;
 
     class Split {
-        use ConfigTrait, LoggerTrait, CurlTrait, SessionTrait;
+        use ConfigTrait, SessionTrait;
 
         /**
          * @param Http\Request $request
@@ -22,14 +19,17 @@ namespace EnterMobileApplication\Controller\Cart {
          */
         public function execute(Http\Request $request) {
             $config = $this->getConfig();
-            $curl = $this->getCurl();
             $session = $this->getSession();
             $cartRepository = new \EnterRepository\Cart();
-            $orderRepository = new \EnterRepository\Order();
-            $productRepository = new \EnterRepository\Product();
 
             // ответ
             $response = new Response();
+
+            // ид региона
+            $regionId = (new \EnterMobileApplication\Repository\Region())->getIdByHttpRequest($request); // FIXME
+            if (!$regionId) {
+                throw new \Exception('Не указан параметр regionId', Http\Response::STATUS_BAD_REQUEST);
+            }
 
             // ид магазина
             $shopId = is_scalar($request->query['shopId']) ? (string)$request->query['shopId'] : null;
@@ -54,158 +54,24 @@ namespace EnterMobileApplication\Controller\Cart {
                 $cartRepository->setProductForObject($cart, $cartProduct);
             }
 
-            /** @var Model\Cart\Product[] $cartProductsById */
-            $cartProductsById = []; // товары в корзине по ид
-            foreach ($cart->product as $cartProduct) {
-                $cartProductsById[$cartProduct->id] = $cartProduct;
-            }
+            // контроллер
+            $controller = new \EnterAggregator\Controller\Cart\Split();
+            // запрос для контроллера
+            $controllerRequest = $controller->createRequest();
+            $controllerRequest->regionId = $regionId;
+            $controllerRequest->shopId = $shopId;
+            $controllerRequest->changeData = $changeData;
+            $controllerRequest->previousSplitData = $previousSplitData;
+            $controllerRequest->cart = $cart;
+            // при получении данных о разбиении корзины - записать их в сессию немедленно
+            $controllerRequest->splitReceivedSuccessfullyCallback->handler = function() use (&$controllerRequest, &$config, &$session) {
+                $session->set($config->order->splitSessionKey, $controllerRequest->splitReceivedSuccessfullyCallback->splitData);
+            };
+            // ответ от контроллера
+            $controllerResponse = $controller->execute($controllerRequest);
 
-            // ид региона
-            $regionId = (new \EnterMobileApplication\Repository\Region())->getIdByHttpRequest($request); // FIXME
-            if (!$regionId) {
-                throw new \Exception('Не указан параметр regionId', Http\Response::STATUS_BAD_REQUEST);
-            }
-
-            // запрос региона
-            $regionQuery = new Query\Region\GetItemById($regionId);
-            $curl->prepare($regionQuery);
-
-            // запрос магазина
-            $shopItemQuery = null;
-            if ($shopId) {
-                $shopItemQuery = new Query\Shop\GetItemById($shopId);
-                $curl->prepare($shopItemQuery)->execute();
-            }
-
-            $curl->execute();
-
-            // регион
-            $region = (new \EnterRepository\Region())->getObjectByQuery($regionQuery);
-
-            // магазин
-            $shop = $shopItemQuery ? (new \EnterRepository\Shop())->getObjectByQuery($shopItemQuery) : null;
-            if ($shopId && !$shop) {
-                $this->getLogger()->push(['type' => 'warn', 'message' => 'Магазин не найден', 'shopId' => $shopId, 'sender' => __FILE__ . ' ' .  __LINE__, 'tag' => ['order.split']]);
-            }
-
-            // запрос на разбиение корзины
-            $splitQuery = new Query\Cart\Split\GetItem(
-                $cart,
-                $region,
-                $shop,
-                null,
-                (array)$previousSplitData,
-                $changeData ? $cartRepository->dumpSplitChange($changeData, $previousSplitData) : []
-            );
-            $splitQuery->setTimeout(10 * $config->coreService->timeout);
-            $curl->prepare($splitQuery);
-
-            $curl->execute();
-
-            // разбиение
-            try {
-                $splitData = $splitQuery->getResult();
-
-                // добавление данных о корзине
-                $splitData['cart'] = [
-                    'product_list' => array_map(
-                        function(Model\Cart\Product $cartProduct) {
-                            return [
-                                'id'       => $cartProduct->id,
-                                'quantity' => $cartProduct->quantity,
-                                'sender'   => $cartProduct->sender,
-                                'meta'     => $cartProduct->clientMeta,
-                            ];
-                        },
-                        $cart->product
-                    ),
-                ];
-
-                // сохранение в сессии
-                $session->set($config->order->splitSessionKey, $splitData);
-
-                $response->split = new Model\Cart\Split($splitData);
-                $response->split->region = $region;
-
-                // MAPI-4
-                $productIds = [];
-                foreach ($response->split->orders as $order) {
-                    foreach ($order->products as $product) {
-                        $product->meta = isset($cartProductsById[$product->id]) ? $cartProductsById[$product->id]->clientMeta : null; // FIXME
-
-                        $productIds[] = $product->id;
-                    }
-                }
-
-                $productListQuery = new Query\Product\GetListByIdList($productIds, $region->id);
-                $curl->prepare($productListQuery);
-
-                $curl->execute();
-
-                // список товаров
-                $productsById = $productListQuery ? $productRepository->getIndexedObjectListByQueryList([$productListQuery]) : [];
-
-                if ($productsById) {
-                    // MAPI-9
-                    // запрос списка медиа для товаров
-                    $descriptionListQuery = new Query\Product\GetDescriptionListByUiList(
-                        array_map(function(Model\Product $product) { return $product->ui; }, $productsById),
-                        [
-                            'media'       => true,
-                            'media_types' => ['main'], // только главная картинка
-                        ]
-                    );
-                    $curl->prepare($descriptionListQuery);
-
-                    $curl->execute();
-
-                    // товары по ui
-                    $productsByUi = [];
-                    call_user_func(function() use (&$productsById, &$productsByUi) {
-                        foreach ($productsById as $product) {
-                            $productsByUi[$product->ui] = $product;
-                        }
-                    });
-
-                    // медиа для товаров
-                    $productRepository->setDescriptionForListByListQuery($productsByUi, $descriptionListQuery);
-
-                    foreach ($response->split->orders as $order) {
-                        foreach ($order->products as $product) {
-                            $product->media = isset($productsById[$product->id]) ? $productsById[$product->id]->media : []; // FIXME
-                        }
-                    }
-                }
-
-                // товары в деталях ошибок
-                try {
-                    foreach ($response->split->errors as $error) {
-                        $productId = !empty($error->detail['product']['id']) ? $error->detail['product']['id'] : null;
-                        /** @var Model\Product|null $product */
-                        $product = ($productId && isset($productsById[$productId])) ? $productsById[$productId] : null;
-
-                        if (!$product) continue;
-
-                        $error->detail['product'] += [
-                            'name'    => $product->name,
-                            'webName' => $product->webName,
-                            'media'   => $product->media,
-                        ];
-                    }
-                } catch (\Exception $e) {
-                    $this->getLogger()->push(['type' => 'warn', 'error' => $e, 'sender' => __FILE__ . ' ' .  __LINE__, 'tag' => ['order.split']]);
-                }
-
-                // type fix
-                foreach ($response->split->orders as $order) {
-                    if (!(bool)$order->groupedPossiblePointIds) {
-                        $order->groupedPossiblePointIds = null;
-                    }
-                }
-
-            } catch (Query\CoreQueryException $e) {
-                $response->errors = $orderRepository->getErrorList($e);
-            }
+            $response->errors = $controllerResponse->errors;
+            $response->split = $controllerResponse->split;
 
             // response
             return new Http\JsonResponse($response);
