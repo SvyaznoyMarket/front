da79fb868f1565ee865b6e479f5f0ee9bd74c6b9
tamili <ta.burlakova@gmail.com>
2015-05-21 14:18:15 +0300

    MSITE-203

--- web/js/module/order.js
+++ web/js/module/order.js
@@ -5,6 +5,20 @@ define(
     function (
         require, $, _, mustache, util
     ) {
+    	console.log('order');
         var $body = $('body');
+
+        $('.js-order-user-submit').click( function() {
+        	var userForm      = $('.js-order-user-form'),
+        		userFormField = $('.js-order-user-field'),
+        		index,
+        		massage;
+
+        	userFormField.each( function ( index ) {
+        		for( var i = 0; i < userForm.data('error').length; i++ ) {
+	        		index = userForm.data('error')[i].field;
+	        	}
+        	})
+        });
     }
 );
