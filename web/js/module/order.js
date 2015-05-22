f4fa923d49cb202d5d162a3a86a76cca1d7ea080
tamili <ta.burlakova@gmail.com>
2015-05-22 10:12:23 +0300

    MSITE-203 user form

--- web/js/module/order.js
+++ web/js/module/order.js
@@ -1,24 +1,86 @@
 define(
     [
-        'require', 'jquery', 'underscore', 'mustache', 'module/util'
+        'require', 'jquery', 'underscore', 'mustache', 'module/util', 'jquery.maskedinput'
     ],
-    function (
+    function(
         require, $, _, mustache, util
     ) {
-    	console.log('order');
-        var $body = $('body');
+        console.log('order');
 
-        $('.js-order-user-submit').click( function() {
-        	var userForm      = $('.js-order-user-form'),
-        		userFormField = $('.js-order-user-field'),
-        		index,
-        		massage;
+        // устанавливаем маску в поле номера телефона
+        $.mask.definitions['x'] = "[0-9]";
+        $('.js-field-phone').mask("+7(xxx)xxx-xx-xx", {
+            placeholder: "+7(xxx)xxx-xx-xx"
+        });
 
-        	userFormField.each( function ( index ) {
-        		for( var i = 0; i < userForm.data('error').length; i++ ) {
-	        		index = userForm.data('error')[i].field;
-	        	}
-        	})
+        var $field   = $('.js-user-field'),
+            errClass = 'textfield-err',
+            massage,
+            index,
+            tmpl,
+            i;
+
+        // убираем маркер ошибки при фокусе на поле
+        $field.focus(function() {
+        	if ( $field.hasClass(errClass) ) {
+                $(this).removeClass(errClass);
+                $(this)
+                    .removeClass(errClass)
+                    .closest('.js-user-wrap').find('.js-field-error').remove();
+            }
+        });
+
+        // запрос прошел успешно
+        function successForm( result ) {
+            console.log('success form');
+            console.log(result);
+
+            // маркируем поля с ошибками
+            if ( result.errors.length ) {
+                $field.each(function(index) {
+                    for (i = 0; i < result.errors.length; i++) {
+                        index = result.errors[i].field;
+                        massage = result.errors[i].name;
+
+                        if ( $(this).data('field-name') == index ) {
+                            tmpl = '<div class="error-text js-field-error">' + massage + '</div>';
+
+                            $(this)
+                                .addClass(errClass)
+                                .closest('.js-user-wrap').prepend(tmpl);
+                        }
+                    }
+                })
+            }
+
+            return false;
+        }
+
+        // обработка ошибок запроса
+        function errorForm( jqXHR, textStatus, errorThrown ) {
+            console.log('error form');
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+
+        // отправляем запрос с данными пользователя
+        $('.js-user-submit').click(function( event ) {
+            event.preventDefault();
+
+            // отправка запроса
+            var $form = $('.js-user-form'),
+            	url   = $form.attr('action');
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: $form.serialize(),
+                error: errorForm,
+                success: successForm
+            });
+
+            return false;
         });
     }
 );
