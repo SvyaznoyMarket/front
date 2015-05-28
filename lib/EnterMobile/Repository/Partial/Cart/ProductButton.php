d87022ac3cc7d24bcf65d2d5577967a45aa355ae
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-05-28 17:42:08 +0300

    MSITE: isset в проверке контекста кнопки

--- lib/EnterMobile/Repository/Partial/Cart/ProductButton.php
+++ lib/EnterMobile/Repository/Partial/Cart/ProductButton.php
@@ -67,11 +67,11 @@ class ProductButton {
             ],
         ];
 
-        if ($context && $context['position'] && $context['position'] == 'listing') {
+        if (isset($context['position']) && $context['position'] == 'listing') {
             $ga = [
                 ['send', 'event', 'm_add_to_basket', 'listing', $product->article, '{product.sum}'],
             ];
-        } elseif ($context && $context['position'] && $context['position'] == 'product') {
+        } elseif (isset($context['position']) && $context['position'] == 'product') {
             $ga = [
                 ['send', 'event', 'm_add_to_basket', 'product', $product->article, '{product.sum}'],
             ];
