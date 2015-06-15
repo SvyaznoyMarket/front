ed5c713ff9ec8baf55ff1025e58307aa6e202b9f
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-06-15 16:14:57 +0300

    MSITE-192: стартовый вариант страницы заказов ЛК

--- lib/EnterMobile/Controller/User/Orders.php
+++ lib/EnterMobile/Controller/User/Orders.php
@@ -3,13 +3,40 @@
 namespace EnterMobile\Controller\User;
 
 use Enter\Http;
+use EnterMobile\ConfigTrait;
+use EnterAggregator\MustacheRendererTrait;
+use EnterAggregator\DebugContainerTrait;
+use EnterAggregator\CurlTrait;
+use EnterMobile\Repository;
+use EnterQuery as Query;
+use EnterMobile\Model\Page\DefaultPage as Page;
 
 class Orders {
 
+    use ConfigTrait, CurlTrait, MustacheRendererTrait, DebugContainerTrait;
+
     public function execute(Http\Request $request) {
+        $curl = $this->getCurl();
+        $user = new \EnterRepository\User();
 
-        $response = new Http\Response('empty orders');
+        $token = $user->getTokenByHttpRequest($request);
 
-        return $response;
+        $ordersQuery = new Query\Order\GetListByUserToken($token, 0, 40);
+        $curl->prepare($ordersQuery);
+        $curl->execute();
+
+        $page = new Page();
+        $page->title = 'Заголовок';
+        $page->content = $ordersQuery->getResult();
+
+        // рендер
+        $renderer = $this->getRenderer();
+        $renderer->setPartials([
+            'content' => 'page/private/orders'
+        ]);
+
+        $content = $renderer->render('layout/default', $page);
+
+        return new Http\Response($content);
     }
 }
