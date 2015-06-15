16adf9adbe1073aefa8d391458101a48e92c62d1
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-06-15 16:12:54 +0300

    MSITE-197: стартовый вариант главной страницы личного кабинета

--- lib/EnterMobile/Controller/User/Index.php
+++ lib/EnterMobile/Controller/User/Index.php
@@ -3,13 +3,28 @@
 namespace EnterMobile\Controller\User;
 
 use Enter\Http;
+use EnterMobile\ConfigTrait;
+use EnterAggregator\MustacheRendererTrait;
+use EnterAggregator\DebugContainerTrait;
+use EnterMobile\Repository;
+use EnterQuery as Query;
+use EnterMobile\Model\Page\DefaultPage as Page;
 
 class Index {
+    use ConfigTrait, MustacheRendererTrait, DebugContainerTrait;
 
     public function execute(Http\Request $request) {
+        $page = new Page();
+        $page->title = 'Заголовок';
 
-        $response = new Http\Response('empty index');
+        // рендер
+        $renderer = $this->getRenderer();
+        $renderer->setPartials([
+            'content' => 'page/private/index'
+        ]);
 
-        return $response;
+        $content = $renderer->render('layout/default', $page);
+
+        return new Http\Response($content);
     }
 }
