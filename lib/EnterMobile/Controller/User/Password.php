a4e5a61a43c70f2f986990a58554e428afde9943
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-06-15 16:18:18 +0300

    MSITE-197: стартовый вариант страницы изменения пароля

--- lib/EnterMobile/Controller/User/Password.php
+++ lib/EnterMobile/Controller/User/Password.php
@@ -3,13 +3,31 @@
 namespace EnterMobile\Controller\User;
 
 use Enter\Http;
+use EnterMobile\ConfigTrait;
+use EnterAggregator\MustacheRendererTrait;
+use EnterAggregator\DebugContainerTrait;
+use EnterAggregator\CurlTrait;
+use EnterMobile\Repository;
+use EnterQuery as Query;
+use EnterMobile\Model\Page\DefaultPage as Page;
 
 class Password {
 
+    use ConfigTrait, CurlTrait, MustacheRendererTrait, DebugContainerTrait;
+
     public function execute(Http\Request $request) {
 
-        $response = new Http\Response('empty password');
+        $page = new Page();
+        $page->title = 'Заголовок';
 
-        return $response;
+        // рендер
+        $renderer = $this->getRenderer();
+        $renderer->setPartials([
+            'content' => 'page/private/password'
+        ]);
+
+        $content = $renderer->render('layout/default', $page);
+
+        return new Http\Response($content);
     }
 }
