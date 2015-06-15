f42c49e27459735117c9811be34bc6972af75b6b
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-06-15 16:19:30 +0300

    MSITE-197: стартовый вариант страницы профиля ЛК

--- lib/EnterMobile/Controller/User/Edit.php
+++ lib/EnterMobile/Controller/User/Edit.php
@@ -3,13 +3,31 @@
 namespace EnterMobile\Controller\User;
 
 use Enter\Http;
+use EnterMobile\ConfigTrait;
+use EnterAggregator\MustacheRendererTrait;
+use EnterAggregator\DebugContainerTrait;
+use EnterAggregator\CurlTrait;
+use EnterMobile\Repository;
+use EnterQuery as Query;
+use EnterMobile\Model\Page\DefaultPage as Page;
 
 class Edit {
 
+    use ConfigTrait, CurlTrait, MustacheRendererTrait, DebugContainerTrait;
+
     public function execute(Http\Request $request) {
 
-        $response = new Http\Response('empty edit');
+        $page = new Page();
 
-        return $response;
+        $page->title = 'Заголовок';
+        // рендер
+        $renderer = $this->getRenderer();
+        $renderer->setPartials([
+            'content' => 'page/private/profile'
+        ]);
+
+        $content = $renderer->render('layout/default', $page);
+
+        return new Http\Response($content);
     }
 }
