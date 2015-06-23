e8ea8611fc2b875b1d56f934c72b1f9d9575c3d5
tamili <ta.burlakova@gmail.com>
2015-06-23 16:19:06 +0300

    MSITE-206

--- web/js/module/search.js
+++ web/js/module/search.js
@@ -4,15 +4,14 @@ define(
         var $body = $('body'),
 
         showPopup = function (e) {
+            var
+                header      = $('.js-header'),
+                searchClass = 'search';
+
+            header.addClass(searchClass);
             e.stopPropagation();
             e.preventDefault();
 
-            console.log('showPopup');
-
-            $('.js-searchWindow').enterPopup({
-                popupCSS : {top: $('.header').height() + 20, marginTop: 0},
-                closeBtn: false
-            });
 
             // MSITE-156
             $($('.js-search-form').data('inputSelector')).focus();
@@ -26,6 +25,33 @@ define(
             if ($input.length && ($input.val().length < 2)) { // FIXME: вынести в data-атрибут
                 e.preventDefault();
             }
-        })
+        });
+
+        var
+            submitSearch = function () {
+                var
+                    searchInputVal = $('.js-search-form-input').val(),
+                    url = '/search/autocomplete?q=' + searchInputVal;
+
+                $.ajax({
+                    type: 'POST',
+                    url: url,
+                    success: successSearch,
+                    error: errorSearch
+                });
+            },
+
+            successSearch = function( result ) {
+                console.log(result);
+            },
+
+            errorSearch = function( jqXHR, textStatus, errorThrown ) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            };
+
+        $body.on('click', 'js-searchLink', showPopup);
+        $('.js-search-form-input').on('change', submitSearch);
     }
 );
