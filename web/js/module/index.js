0044ab44e393496cfb2ea47119c11d0cc80c1f2c
tamili <ta.burlakova@gmail.com>
2015-06-25 18:47:02 +0300

    MSITE-206

--- web/js/module/index.js
+++ web/js/module/index.js
@@ -44,23 +44,18 @@ define(
 	        	arrows: false,
 	        	centerPadding: '150px',
 	        	responsive: [
-			    {
-			      breakpoint: 900,
-			      settings: {
-					dots: true,
-					infinite: true,
-					centerMode: true,
-					slidesToShow: 1,
-		        	slidesToScroll: 1,
-		        	focusOnSelect: true,
-		        	arrows: false,
-		        	centerPadding: '50px',
-			      },
-			      breakpoint: 750,
-			      settings: {
-			      	centerMode: false
-			      }
-			    }
+				    {
+				      breakpoint: 900,
+				      settings: {
+			            centerPadding: '50px'
+				      }
+				    },
+				    {
+				      breakpoint: 750,
+				      settings: {
+				      	centerMode: false
+				      }
+				    }
 			    ]
 			}).parent().removeClass('m-loader');
     }
