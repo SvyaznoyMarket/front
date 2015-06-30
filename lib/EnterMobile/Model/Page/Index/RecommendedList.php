af526eda1e184f40afcd43c21066f3522b26188f
alexander-ponomarev <aleksandr.ponomarev@enter.ru>
2015-06-30 13:21:56 +0300

    MSITE-246: добавил рекомендации для главной страницы

---
+++ lib/EnterMobile/Model/Page/Index/RecommendedList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EnterMobile\Model\Page\Index {
+    use EnterMobile\Model\Partial;
+
+    class RecommendedList {
+        /**
+         * Виджеты, индексированные по css-селектору
+         * @var Partial\Widget[]
+         */
+        public $widgets = [];
+    }
+}
