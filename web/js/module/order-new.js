1d02d557b4c679bf817a0ba15a6e9b3c656c0f46
g84 <green.840429@gmail.com>
2015-06-30 16:20:23 +0300

    MSITE-203 добавил лоадер на первом шаге

--- web/js/module/order-new.js
+++ web/js/module/order-new.js
@@ -6,7 +6,9 @@ define(
         require, $, _, mustache, util, formValidator, analytics
     ) {
         var
-            $form = $('.js-user-form')
+            $form         = $('.js-user-form'),
+            $orderContent = $('.js-order-content'),
+            loaderClass   = 'm-loader'
         ;
 
         // отправляем запрос с данными пользователя
@@ -15,32 +17,36 @@ define(
             event.preventDefault();
 
             // отправка запроса
-            var $form = $('.js-user-form'),
-            	url   = $form.attr('action');
+            var
+                $form = $('.js-user-form'),
+            	url   = $form.attr('action')
+            ;
 
             $.ajax({
                 type: 'POST',
                 url: url,
-                data: $form.serialize(),
-                error: function() {
-                    analytics.push(['6_2 Далее_ошибка_Получатель']);
-                },
-                success: function(result) {
-                    formValidator.validate($form, result.errors);
+                data: $form.serialize()
+            }).done(function(response) {
+                formValidator.validate($form, response.errors);
 
-                    // если ошибок нет переход на следущий шаг
-                    if (result.redirect != null && result.redirect.length) {
-                        analytics.push(['6_1 Далее_успешно_Получатель_ОБЯЗАТЕЛЬНО']);
+                // если ошибок нет переход на следущий шаг
+                if (response.redirect != null && response.redirect.length) {
+                    analytics.push(['6_1 Далее_успешно_Получатель_ОБЯЗАТЕЛЬНО']);
 
-                        window.location.href = result.redirect;
-                    }
-
-                    try {
-                        if (result.errors && result.errors.length) {
-                            analytics.push(['6_2 Далее_ошибка_Получатель', 'Поле ошибки: ' + _.map(result.errors, function(error) { return error.field; })]);
-                        }
-                    } catch (error) { console.error(error); }
+                    window.location.href = response.redirect;
+                    $orderContent.addClass(loaderClass);
+                    setTimeout(function() { $orderContent.removeClass(loaderClass); }, 10000);
                 }
+
+                try {
+                    if (response.errors && response.errors.length) {
+                        analytics.push(['6_2 Далее_ошибка_Получатель', 'Поле ошибки: ' + _.map(response.errors, function(error) { return error.field; })]);
+                    }
+                } catch (error) { console.error(error); }
+            }).always(function() {
+                console.info('unblock screen');
+            }).error(function(xhr, textStatus, error) {
+                analytics.push(['6_2 Далее_ошибка_Получатель']);
             });
 
         });
