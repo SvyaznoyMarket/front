81ec0d0c8a8e8479928585abe62be1195f7b07fb
frontgirl <web@mow-01-ic237.enter.lan>
2015-07-13 15:28:33 +0300

    MSITE-362 новое меню js

--- web/js/build/module/navigation.js
+++ web/js/build/module/navigation.js
@@ -1,1 +1,57 @@
-define(["jquery","jquery.scrollTo","snap"],function(e){var o=e(".chooseModel"),n=o.find(".chooseModel_moreLink"),i=o.find(".chooseModel_moreBox"),t=(o.find(".chooseModel_moreBox.more"),function(){i.slideToggle("800"),n.toggleClass("more")}),l=new Snap({element:document.getElementById("wrapper"),disable:"right"});n.click(t);var s=e("body"),a=(e(".js-fader"),e(".js-nav-open-link"));e(a).on("click",function(e){e.preventDefault(),s.hasClass("snapjs-left")?l.close():l.open("left")});var r=function(){var o=e(".footer");o.css("position",s.height()+o.innerHeight()>e(window).height()?"inherit":"fixed")};e(window).on("load resize",r),e(document).on("focus","input, textarea, input + label, select",function(){s.addClass("fixfixed")}).on("blur","input, textarea, input + label, select",function(){s.removeClass("fixfixed")})});
+define(
+    ['jquery', 'jquery.scrollTo', 'snap'],
+    function ($) {
+
+        var chooseModelWrap = $('.chooseModel'),
+            chooseModelMoreLink = chooseModelWrap.find('.chooseModel_moreLink'),
+            chooseModelMoreBox = chooseModelWrap.find('.chooseModel_moreBox'),
+            chooseModelMoreBoxDown = chooseModelWrap.find('.chooseModel_moreBox.more'),
+
+            chooseModelMoreModel = function chooseModelMoreModel() {
+                chooseModelMoreBox.slideToggle('800');
+                chooseModelMoreLink.toggleClass('more');
+            },
+            snapper = new Snap({
+                element: document.getElementById('wrapper'),
+                disable: 'right'
+            })
+            ;
+        // end of vars
+
+        chooseModelMoreLink.click(chooseModelMoreModel);
+
+        /**
+         * Навигация сайта, показывается при клике по иконке .navIco
+         */
+        var
+            body = $('body'),
+            fader = $('.js-fader'),
+            navIco = $('.js-nav-open-link'),
+
+            showHideMenu = function showHideMenu(e) {
+
+                e.preventDefault();
+
+                body.hasClass('snapjs-left') ? snapper.close() : snapper.open('left');
+            }),
+
+            footerResize = function footerResize() {
+                var
+                    footer = $('.footer');
+
+                footer.css('position', body.height() + footer.innerHeight() > $(window).height() ? 'inherit' : 'fixed');
+            };
+        // end of vars
+
+        $(window).on('load resize', footerResize);
+        $(navIco).on('click', showHideMenu);
+
+        $(document)
+            .on('focus', 'input, textarea, input + label, select', function(e) {
+                body.addClass('fixfixed');
+            })
+            .on('blur', 'input, textarea, input + label, select', function(e) {
+                body.removeClass('fixfixed');
+            });
+    }
+);
--- web/js/module/navigation.js
+++ web/js/module/navigation.js
@@ -26,84 +26,15 @@ define(
         var
             body = $('body'),
             fader = $('.js-fader'),
-            navIco = $('.js-nav-open-link');
-
-        $(navIco).on('click',function(e){
-
-            e.preventDefault();
-
-            body.hasClass('snapjs-left') ? snapper.close() : snapper.open('left');
-        });
-
-        /*var
-            body = $('body'),
             navIco = $('.js-nav-open-link'),
-            navSite = $('.js-site-nav'),
-            navSiteLeft = navSite.width(),
 
-            fader = $('.js-fader'),
-
-            navSiteItemLevel1 = navSite.find('.navList_text'),
-            navSiteListLevel2 = navSite.find('.navListLevel2');
-        // end of vars
-
-        navSite.css({'left' : -navSiteLeft});
-        navSiteListLevel2.hide();*/
-
-        var
-            /**
-             * Показываем/скрываем навигацию
-             *//*
-            slideNav = function slideNav(e) {
-                if ( navSite.css('display') == 'block' ) {
-                    closeNav();
-                }
-
-                else {
-                    fader.show(0);
-                    navSite.stop(true, true).show(0).animate({'left' : 0},300);
-                    $('html').addClass('no-scroll');
-                }
+            showHideMenu = function showHideMenu(e) {
 
                 e.preventDefault();
-            },
 
-            /**
-             * Показываем/скрываем навигацию второго уравня
-             *//*
-            slideNavLevel2 = function slideNavLevel2() {
-                if ($(this).data('url')) {
-                    window.location.href = $(this).data('url');
+                body.hasClass('snapjs-left') ? snapper.close() : snapper.open('left');
+            }),
 
-                    return false;
-                }
-
-                navSiteListLevel2.slideUp();
-
-                if ( ($(this).next(navSiteListLevel2)).is(':visible') ) {
-                    navSiteListLevel2.slideUp();
-                    return;
-                }
-
-                $(this).next(navSiteListLevel2).stop(true, false).slideDown();
-
-                if ($(this).parent().is(":last-child")) {
-                    navSite.animate({ scrollTop: navSite[0].scrollHeight }, 1000);
-                }
-
-                return false;
-            },
-
-            /**
-             * Скрываем навигацию при клике в любом месте кроме .nav
-             *//*
-            closeNav = function closeNav() {
-                fader.hide(0);
-                $('html').removeClass('no-scroll');
-                navSite.stop(true, true).animate({'left' : -navSiteLeft},300).hide(0);
-                navSiteListLevel2.slideUp();
-            },
-*/
             footerResize = function footerResize() {
                 var
                     footer = $('.footer');
@@ -111,13 +42,9 @@ define(
                 footer.css('position', body.height() + footer.innerHeight() > $(window).height() ? 'inherit' : 'fixed');
             };
         // end of vars
-/*
-        navIco.on('click', slideNav);
-        navSiteItemLevel1.on('click', slideNavLevel2);*/
-        /*fader.live('click touchend', function(){
-            snapper.close;
-        })*/;
+
         $(window).on('load resize', footerResize);
+        $(navIco).on('click', showHideMenu);
 
         $(document)
             .on('focus', 'input, textarea, input + label, select', function(e) {
