098a141ed87bfb64ea17b8aa94fcbde45b2572fb
frontgirl <web@mow-01-ic237.enter.lan>
2015-08-24 15:42:21 +0300

    MSITE-496 fix

--- web/js/plugin/heightFixIos.js
+++ web/js/plugin/heightFixIos.js
@@ -134,44 +134,4 @@ define(
 
         iOSFix.init();
 
-        /*footerResize();
-
-        //check if iOS7 & fix body height
-        if (
-            navigator.userAgent.match(/iPad;.*CPU.*OS 7_\d/i) &&
-            window.innerHeight != document.documentElement.clientHeight
-        ) {
-            var fixViewportHeight = function() {
-                var wh = $(window).height(),
-                    ww = $(window).width(),
-                    body = $('body');
-
-                document.documentElement.style.height = $(window).innerHeight + "px";
-                body.removeAttr('style');
-
-                if (document.body.scrollTop !== 0) {
-                    $(window).scrollTo(0, 0);
-                }
-                if (wh < ww){
-                    //landscape position
-
-                    body.height(wh - 20);
-                }
-            };
-
-            //alert('ios7!!!');
-            window.addEventListener("scroll", fixViewportHeight, false);
-            window.addEventListener("orientationchange", function(){
-                fixViewportHeight();
-                footerResize();
-            }, false);
-
-            fixViewportHeight();
-
-            document.body.style.webkitTransform = "translate3d(0,0,0)";
-        } else {
-            $(window).on("load resize", footerResize);
-        }*/
-
-
     });
