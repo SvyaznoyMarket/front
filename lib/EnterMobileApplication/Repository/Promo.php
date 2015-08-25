a193bb7852a4d9ea7f72d676dd8518a6d38b8e01
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-08-25 11:52:44 +0300

    MAPI-102 Не возвращать баннеры с тегом отличным от mobile

--- lib/EnterMobileApplication/Repository/Promo.php
+++ lib/EnterMobileApplication/Repository/Promo.php
@@ -29,20 +29,24 @@ class Promo extends \EnterRepository\Promo {
                 unset($promo->target->categoryToken);
             }
 
-            // MAPI-102
-            foreach ($promo->media->photos as $mediaKey => $media) {
-                if (!in_array('mobile', $media->tags, true)) {
-                    unset($promo->media->photos[$mediaKey]);
+            // MAPI-102 Не возвращать баннеры с тегом отличным от mobile
+            call_user_func(function() use(&$promos, $promoKey, $promo) {
+                foreach ($promo->media->photos as $mediaKey => $media) {
+                    if (!in_array('mobile', $media->tags, true)) {
+                        unset($promo->media->photos[$mediaKey]);
+                    }
                 }
-            }
 
-            $promo->media->photos = array_values($promo->media->photos);
+                $promo->media->photos = array_values($promo->media->photos);
 
-            if (!$promo->media->photos) {
-                unset($promos[$promoKey]);
-            }
+                if (!$promo->media->photos) {
+                    unset($promos[$promoKey]);
+                }
+
+                $promos = array_values($promos);
+            });
         }
         
-        return array_values($promos);
+        return $promos;
     }
 }
