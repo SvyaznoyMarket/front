7ac52cf4f523eaa9ead447a1d1527423e3e3a703
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-08-28 11:08:32 +0300

    MAPI-105 Заменить получение контентных страниц из wordpress на получение из scms

--- lib/EnterQuery/Content/GetItemByToken.php
+++ lib/EnterQuery/Content/GetItemByToken.php
@@ -3,24 +3,23 @@
 namespace EnterQuery\Content;
 
 use Enter\Curl\Query;
-use EnterQuery\ContentQueryTrait;
+use EnterQuery\ScmsQueryTrait;
 use EnterQuery\Url;
 
 class GetItemByToken extends Query {
-    use ContentQueryTrait;
+    use ScmsQueryTrait;
 
     /** @var array|null */
     protected $result;
 
     /**
      * @param string $token
-     * @param bool $showSidebar
      */
-    public function __construct($token, $showSidebar = true) {
+    public function __construct($token) {
         $this->url = new Url();
-        $this->url->path = $token;
+        $this->url->path = 'api/static-page';
         $this->url->query = [
-            'show_sidebar' => (int)$showSidebar,
+            'token' => [$token],
         ];
 
         $this->init();
@@ -32,6 +31,6 @@ class GetItemByToken extends Query {
     public function callback($response) {
         $data = $this->parse($response);
 
-        $this->result = isset($data['content']) ? $data : null;
+        $this->result = isset($data['pages'][0]) ? $data['pages'][0] : null;
     }
 }
