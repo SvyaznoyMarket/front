e79511ba5e93fe5f6a5153f17bf6a8bd6b29b0de
g84 <green.840429@gmail.com>
2015-10-15 17:32:52 +0300

    MSITE-519 Сердечко у НЕзалогиненного пользователя

--- web/js/module/config.js
+++ web/js/module/config.js
@@ -1,6 +1,15 @@
 define(
     ['jquery', 'underscore'],
     function ($, _) {
+        $.ajaxSetup({
+            timeout: 30000,
+            statusCode: {
+                401: function () {
+                    window.location.href = '/login?redirect_to=' + window.location.pathname;
+                }
+            }
+        });
+
         return _.extend({
             cookie: {
                 domain: null,
