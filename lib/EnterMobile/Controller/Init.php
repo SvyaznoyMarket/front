dd7c71c3d3e070a509203cefaab5d0fe069b8891
enter-m-haritonov <mail@m-haritonov.net>
2017-05-12 15:19:48 +0300

    MSITE-641 Переход сайта на защищенный протокол https (последний этап)

--- lib/EnterMobile/Controller/Init.php
+++ lib/EnterMobile/Controller/Init.php
@@ -17,71 +17,84 @@ class Init {
      * @param Http\Response $response
      */
     public function execute(Http\Request $request, Http\Response &$response = null) {
-        $curl = $this->getCurl();
+        $redirectUrl = call_user_func(function() use($request) {
+            $curl = $this->getCurl();
 
-        $url = $request->getPathInfo();
+            $url = $request->getPathInfo();
 
-        $redirectQuery = null;
-        if (
-            $this->getConfig()->redirectManager->enabled
-            && !$request->isXmlHttpRequest()
-            && ('/' !== $url)
-            && (0 !== strpos($url, '/orders/'))
-            && (0 !== strpos($url, '/order/'))
-            && (0 !== strpos($url, '/cart'))
-            && (0 !== strpos($url, '/private'))
-        ) {
-            $redirectQuery = new Query\RedirectManager\GetItem($url);
-            $curl->prepare($redirectQuery);
+            $redirectQuery = null;
+            if (
+                $this->getConfig()->redirectManager->enabled
+                && !$request->isXmlHttpRequest()
+                && ('/' !== $url)
+                && (0 !== strpos($url, '/orders/'))
+                && (0 !== strpos($url, '/order/'))
+                && (0 !== strpos($url, '/cart'))
+                && (0 !== strpos($url, '/private'))
+            ) {
+                $redirectQuery = new Query\RedirectManager\GetItem($url);
+                $curl->prepare($redirectQuery);
+            }
+
+            $abTestQuery = new Query\AbTest\GetActiveList();
+            $curl->prepare($abTestQuery);
+
+            $curl->execute();
+
+            try {
+                $this->getAbTest()->setObjectListByQuery($abTestQuery);
+                $this->getAbTest()->setValueForObjectListByHttpRequest($request);
+            } catch(\Exception $e) {
+                $this->getLogger()->push(['type' => 'error', 'error' => $e, 'sender' => __FILE__ . ' ' .  __LINE__, 'tag' => ['abtest']]);
+            }
+
+            // если не было запроса на получение редиректа или произошла ошибка...
+            if (!$redirectQuery || $redirectQuery->getError()) {
+                return;
+            }
+
+            $result = (array)$redirectQuery->getResult() + ['to_url' => null];
+            $redirectUrl = trim($result['to_url']);
+
+            if (!$redirectUrl) {
+                return;
+            }
+
+            if (0 !== strpos($redirectUrl, '/')) {
+                $this->getLogger()->push([
+                    'type' => 'warn',
+                    'error' => sprintf('Неправильный редирект %s -> %s', $url, $redirectUrl),
+                    'sender' => __FILE__ . ' ' .  __LINE__,
+                    'tag' => ['redirect']
+                ]);
+
+                return;
+            }
+
+            if (false === strpos($redirectUrl, '?') && $request->getQueryString()) {
+                $redirectUrl .= '?' . $request->getQueryString();
+            }
+
+            return $request->getSchemeAndHttpHost() . $redirectUrl;
+        });
+
+        if (!$request->isSecure()) {
+            if (!$redirectUrl) {
+                $redirectUrl = $request->getUri();
+            }
+
+            $redirectUrl = preg_replace('/^http:/is', 'https:', $redirectUrl);
         }
-
-        $abTestQuery = new Query\AbTest\GetActiveList();
-        $curl->prepare($abTestQuery);
-
-        $curl->execute();
-
-        try {
-            $this->getAbTest()->setObjectListByQuery($abTestQuery);
-            $this->getAbTest()->setValueForObjectListByHttpRequest($request);
-        } catch(\Exception $e) {
-            $this->getLogger()->push(['type' => 'error', 'error' => $e, 'sender' => __FILE__ . ' ' .  __LINE__, 'tag' => ['abtest']]);
-        }
-
-        // если не было запроса на получение редиректа или произошла ошибка...
-        if (!$redirectQuery || $redirectQuery->getError()) {
-            return;
-        }
-
-        $result = (array)$redirectQuery->getResult() + ['to_url' => null];
-        $redirectUrl = trim($result['to_url']);
 
         if (!$redirectUrl) {
             return;
         }
 
-        if (0 !== strpos($redirectUrl, '/')) {
-            $this->getLogger()->push([
-                'type' => 'warn',
-                'error' => sprintf('Неправильный редирект %s -> %s', $url, $redirectUrl),
-                'sender' => __FILE__ . ' ' .  __LINE__,
-                'tag' => ['redirect']
-            ]);
-
-            return;
-        }
-
-        if (false === strpos($redirectUrl, '?') && $request->getQueryString()) {
-            $redirectUrl .= '?' . $request->getQueryString();
-        }
-
-        $statusCode = Http\Response::STATUS_MOVED_PERMANENTLY;
-        $redirectUrl = $request->getSchemeAndHttpHost() . $redirectUrl;
-
         $this->getLogger()->push(['redirect' => [
             'url'   => $redirectUrl,
-            'code'  => $statusCode,
+            'code'  => Http\Response::STATUS_MOVED_PERMANENTLY,
         ], 'sender' => __FILE__ . ' ' .  __LINE__, 'tag' => ['request']]);
 
-        $response = (new \EnterAggregator\Controller\Redirect())->execute($redirectUrl, $statusCode);
+        $response = (new \EnterAggregator\Controller\Redirect())->execute($redirectUrl, Http\Response::STATUS_MOVED_PERMANENTLY);
     }
 }
